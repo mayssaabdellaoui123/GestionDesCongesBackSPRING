312ade6e5e91197506aca9efef167c1c56c94f57
mayssaabdellaoui123 <58725994+mayssaabdellaoui123@users.noreply.github.com>
2021-09-24 16:29:47 +0200

    kk

--- src/main/java/ConsomiTounsi/Service/CongeManager.java
+++ src/main/java/ConsomiTounsi/Service/CongeManager.java
@@ -1,8 +1,34 @@
 package ConsomiTounsi.Service;
 
+import ConsomiTounsi.entities.Admin;
+import ConsomiTounsi.entities.Conge;
+import ConsomiTounsi.entities.Departement;
+import ConsomiTounsi.entities.User;
+import ConsomiTounsi.repository.DepartementRepository;
+import ConsomiTounsi.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 
 public class CongeManager implements CongeManagerInterface {
+
+    @Autowired
+    UserRepository ur;
+
+    @Autowired
+    DepartementRepository dr;
+
+
+    public List<Conge> getCongeChefDepartment(String username){
+
+        Admin a = (Admin) ur.findByUsernameUser(username).get();
+        String matricule = a.getMatricule();
+        List<Long> departmentname = dr.getIdDepartmentByMatriculeBoss(matricule);
+
+
+
+    }
 }
--- src/main/java/ConsomiTounsi/repository/CongeRepository.java
+++ src/main/java/ConsomiTounsi/repository/CongeRepository.java
@@ -3,8 +3,17 @@ package ConsomiTounsi.repository;
 import ConsomiTounsi.entities.Conge;
 import ConsomiTounsi.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CongeRepository extends JpaRepository<Conge,Long> {
+
+    @Query("SELECT c FROM Conge c WHERE c.validationPrimaire= :fn AND u.validationFinale = :ln AND u.validationFinale = :ln" )
+    List<User> RetiveUserByFirstAndLastNameJPQL(@Param("fn") String fn , @Param("ln") String ln);
+
+
 }
